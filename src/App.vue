e45f69d1f5cf5f09d575757704ba0c3c2a2ac513
nakashimashin <nakashimashin0517@gmail.com>
2023-10-02 23:43:45 +0900

    Headerコンピュータを追加

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Header from '@/components/Header.vue'
 </script>
 
 <template>
-  <router-view />
+  <div>
+    <Header />
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="body-header">Header</div>
+  </div>
+</template>
+
+<style scoped>
+.body-header{
+    width: 100%;
+    height: 20%;
+    background-color: red;
+}
+</style>
