cbc364a60f5eb004e91ead38db844a3fd72450d0
nakashimashin <nakashimashin0517@gmail.com>
2023-10-04 01:20:07 +0900

    difineStoreの中でstate,actions,gettersのプロパティを利用

---
+++ src/pages/Home2.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useStoreCounter2 } from '../stores/counter2';
+const counter = useStoreCounter2();
+</script>
+
+<template>
+  <div>
+    <div>Home2</div>
+    <p>Count:{{ counter.count }}</p>
+    <p>DoubleCount: {{ counter.doubleCount }}</p>
+    <button @click="counter.increment()">Up</button>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '@/pages/Home.vue'
-import Error from '@/pages/Error.vue'
+import Home from '@/pages/Home.vue';
+import Home2 from '@/pages/Home2.vue'
+import Error from '@/pages/Error.vue';
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
+    {path: '/home2', name: 'Home2', component: Home2},
     {path: '/:catchAll(.*)', name: 'Error', component: Error},
 ]
 
