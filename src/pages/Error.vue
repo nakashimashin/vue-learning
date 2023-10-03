4190b391a34619ce7041c89548d82e58f5c1475a
nakashimashin <nakashimashin0517@gmail.com>
2023-10-03 10:41:34 +0900

    404エラー画面作成

---
+++ src/pages/Error.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div>このページは存在しません</div>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from './pages/Home.vue'
+import Home from '@/pages/Home.vue'
+import Error from '@/pages/Error.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
+    {path: '/:catchAll(.*)', name: 'Error', component: Error},
 ]
 
 const router = createRouter({
