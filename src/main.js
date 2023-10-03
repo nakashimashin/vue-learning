025d0ed1da4011eea8e2f0765b666c755d399059
nakashimashin <nakashimashin0517@gmail.com>
2023-10-03 11:02:20 +0900

    piniaをインストール

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import 'normalize.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
+import 'normalize.css';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+const pinia = createPinia();
+app.use(router,pinia);
+app.mount('#app');
